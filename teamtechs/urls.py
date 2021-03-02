fb557ff63a2c085ea1957b210bcaf799b4ad1027
Onjomba Felix <73028636+Developer-Felix@users.noreply.github.com>
2021-03-02 10:32:40 -0800

    Update urls.py
    update new learning sessions

--- teamtechs/urls.py
+++ teamtechs/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('signup/',views.signup,name='signup'),
 	path('accounts/',include('django.contrib.auth.urls')),
+	path('codewithteamtechkenya/',views.codewithttk,name='codewithttk'),
+	path('learningsessions/',views.registerwithttk,name='registerwithttk'),
 ]
